b8632f621d4458e3c793b43e4db89353b2bb4254
LabCom <LabCom@DESKTOP-R4U7GJA>
2016-10-05 15:08:45 -0500

    index

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Electivo II</title>
+    </head>
+    <body>
+        <form method="POST">
+            <fieldset>
+                <legend>:: ELECTIVO II</legend>
+                <table>
+                    <tr>
+                        <td>Codigo: </td>
+                        <td>
+                            <input type="text" size="10" name="txtCodigo" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Nombre: </td>
+                        <td>
+                            <input type="text" size="10" name="txtNombre" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Horas: </td>
+                        <td>
+                            <input type="text" size="10" name="txtHoras" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Credito: </td>
+                        <td>
+                            <input type="text" size="10" name="txtCredito" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name="btnEnviar" value="Enviar"/>
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+    </body>
+</html>
+        <?php
+       
+        
+        ?>
+    </body>
+</html>
